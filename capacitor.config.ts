c8c1bde892c5056dd5cb853b3ea84bd1b287ac4b
Roger <roger@gmail.com>
2026-02-08 19:23:31 +0200

    single shot clean rebuild with web entry

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,18 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.snapdb.app',
   appName: 'SnapDB',
-  webDir: 'www',
-  bundledWebRuntime: false,
-  android: {
-    allowMixedContent: true
-  },
-  plugins: {
-    CapacitorFilePicker: {},
-    Media: {},
-    Filesystem: {}
-  }
+  webDir: 'dist-app'
 };
 
 export default config;
